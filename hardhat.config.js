2fbadbe7843e374d7f47f883971f5104b26bba49
Luja Chitrakar <lujachitrakar156@gmail.com>
2025-02-11 11:08:39 +0545

    deposit function

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    // sepolia: {
+    //   url: SEPOLIA_URL,
+    //   accounts: [SEPOLIA_PRIVATE_KEY],
+    // },
+  },
 };
